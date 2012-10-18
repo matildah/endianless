f9fa3851af7e0b43b1b13aa9f9ce6b37a92d43f3
Kia <2592730+superbaud@users.noreply.github.com>
2012-10-18 14:51:36 -0700

    added readme, gitignore, and template file for memcpy routine

---
+++ memcpy.s
@@ -0,0 +1,36 @@
+ZERO    DB  0000 ; all bits zero
+ONES    DB  FFFF ; all bits one
+
+
+
+
+
+T1 DB  00 ; temporary variable
+T2     DB  00 ; temporary variable
+
+dest DB 00 ; result goes here.
+
+memcpy:
+
+NAND ZERO
+NAND ONES
+
+
+ADD Atemp ; A
+NAND Btemp ; A nand B
+ST T1 ; A nand B 
+NAND Atemp ; (A nand B) nand A 
+ST T2 ; (A nand B) nand A 
+NAND ZERO ; FF 
+NAND ONES ; 00 
+ADD T1 ;  A nand B
+NAND Btemp ; (this is t3) (A nand B) nand B
+NAND T2 ; 
+ST rtemp
+
+; t1 = A nand B
+; t2 = A nand t1
+; t3 = B nand t1
+; R = t2 nand t3
+
+
