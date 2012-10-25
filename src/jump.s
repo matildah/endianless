6ff62a0e62e70850afa808cb0c999ad57188c41b
Kia <2592730+superbaud@users.noreply.github.com>
2012-10-25 16:29:12 -0700

    introduced fixed-up (preassembled with the jump instruction) addresses

--- src/jump.s
+++ src/jump.s
@@ -4,6 +4,7 @@ ONE     DB  0001 ; one!
 JNCINST DB  4000 ; opcode for JNC
 hell    DB  3fff ; the address that halts execution when we jump to 
 dest    DB  0000 
+fdest   DB  0000 ; dest + 1 + 0x4000 -- we can do this fixup at assembly-time
 
 jump: ;jumps to the address held in the double byte at dest
 NAND ZERO ; acc = 0xffff
@@ -17,7 +18,7 @@ JNC dest
 
 JNCINSTPLUS DB 4001 ; opcode for JNC plus one
 
-jump2:   ; does the same, but differently
+jump2:   ; does the same, but with one less instruction
 NAND ZERO ; acc = 0xffff
 ADD JNCINSTPLUS ; acc = 0x4000, carry flag is set
 ADD dest 
@@ -25,4 +26,14 @@ JNC hell ; won't jump, carry flag is set, but this UNSETS it
 ST jinst2
 jinst2:
 JNC hell ; will get fixed up, don't worry. 
+
+
+jump3:  ; takes fixed up "addresses" (actually pre-assembled jump instructions)
+
+NAND ZERO
+ADD dest
+JNC hell
+ST jinst3
+jinst3:
+JNC hell
 
