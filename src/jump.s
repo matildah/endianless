17f858666063bcbb82560eb3b570f73a43c0da7c
Kia <2592730+superbaud@users.noreply.github.com>
2012-10-26 16:17:00 -0700

    restructed jump.s to be more readable, added comments

--- src/jump.s
+++ src/jump.s
@@ -6,7 +6,40 @@ hell    DB  3fff ; the address that halts execution when we jump to
 dest    DB  0000 
 fdest   DB  0000 ; dest + 1 + 0x4000 -- we can do this fixup at assembly-time
 
-jump: ;jumps to the address held in the double byte at dest
+
+JNCINSTPLUS DB 4001 ; opcode for JNC plus one
+
+jump:           ; jumps to the address stored at dest
+NAND ZERO       ; acc = 0xffff
+ADD JNCINSTPLUS ; acc = 0x4000, carry flag is set
+ADD dest        ; acc is now the correct jump instruction 
+ST jinst2       ; ...which is now stored where we'll execute it
+JNC hell        ; won't jump, carry flag is set, but this UNSETS it
+jinst2:
+JNC hell        ; will get fixed up, don't worry. 
+
+
+fjump:     ; takes fake "addresses" -- the jump opcode 0x4000 plus the 
+           ; destination plus one (so when added to 0xffff, it creates the
+           ; correct jump instruction)
+           ; 
+           ; When addresses are known at compile time, this allows to save one
+           ; instruction
+
+
+NAND ZERO  ; acc = 0xffff
+ADD dest   ; acc = dest - 1, carry flag gets set
+ST jinst3  ; stores acc, so we can execute it
+JNC hell   ; does not jump. unsets carry flag
+jinst3:
+JNC hell   ; acc gets stored here and gets executed
+
+
+
+
+
+jump3: ;jumps to the address held in the word at dest. more efficient version 
+       ; exists in the "jump" routine
 NAND ZERO ; acc = 0xffff
 ADD ONE ; acc = 0x0000 *and* carry flag IS SET
 JNC hell; will not jump because carry flag is set, but UNSETS carry flag
@@ -15,25 +48,3 @@ ADD dest
 ST jinst
 jinst: ; the actual jump opcode is here
 JNC dest
-
-JNCINSTPLUS DB 4001 ; opcode for JNC plus one
-
-jump2:   ; does the same, but with one less instruction
-NAND ZERO ; acc = 0xffff
-ADD JNCINSTPLUS ; acc = 0x4000, carry flag is set
-ADD dest 
-JNC hell ; won't jump, carry flag is set, but this UNSETS it
-ST jinst2
-jinst2:
-JNC hell ; will get fixed up, don't worry. 
-
-
-jump3:  ; takes fixed up "addresses" (actually pre-assembled jump instructions)
-
-NAND ZERO
-ADD dest
-JNC hell
-ST jinst3
-jinst3:
-JNC hell
-
