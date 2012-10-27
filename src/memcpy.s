b45c2344faac1cfb3443b92e42d4c81b345c5f30
Kia <2592730+superbaud@users.noreply.github.com>
2012-10-26 19:11:24 -0700

    started analysis of how to best implement for loops

--- src/memcpy.s
+++ src/memcpy.s
@@ -1,4 +1,6 @@
 ; memcpy -- copies n words from src to dest
+; oh, and also this is where I try to figure out how to implement a for loop 
+; with the fewest instructions :-P
 
 ;constants
 
@@ -16,15 +18,48 @@ dest     DB  5678 ;
 n        DB  0010 ;
 
 
-nprime   DB  0000 ; variable we iterate over. basically, instead of going from
-                 ; zero to n, we go from 2^16-n to zero, and that allows us to 
-                 ; use the carry flag (which gets set only when we increment
-                 ; 2^16 - 1) to detect when we ned to exit the loop.
+nprime   DB  0000 
 
 ; memcpy is basically:
 ; for (int i = 0; i < n ; i++)
 ;   *(dst + i) = *(src + i)
 ;
+
+
+
+; How we implement "for (int i = 0; i < b; i++) {X(i)}" loops in endianless:
+
+; the only kind of jump we have in endianless is jump-if-carry-not-set, and we
+; can combine that with the increment needed in the for loop. We have two 
+; outcomes -- do the loop or exit the loop, and that maps onto carry not set / 
+; carry set. The reason we do it this way rather than the other way around is 
+; that we can set things up such that the loop counter increments from 0xffff
+; on the exit-the-loop case and sets a carry, and the normal case does not set
+; a carry. Namely, you can't increment something, and have it generate a 
+; carry, and increment it again, and have it generate a carry again. 
+; 
+; We have the additional requirement that we be able to have the current value 
+; of i (not i prime) inside a variable (it's needed to do relative addressing)
+; 
+; Given anything inside acc, we can increment i with NAND ZERO, ADD 0x0002,
+; ADD i, ST i. however, if we have something which is related to i with a 
+; known offset, we can do this with less instructions:
+; ADD [the right constant], ST i.
+; 
+; We have a choice though -- how do we order the increment, the test, and the
+; loop body? 
+; 
+; Also, we have basically two choices for how to move the loop index -- from
+; zero to b or from 0xffff - b to zero. 
+; 
+; 
+; 
+; 
+;
+;
+;
+;
+
 
 
 
