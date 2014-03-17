d3ced626b440533fa88c7ade57248c183bf2cca4
Kia <2592730+superbaud@users.noreply.github.com>
2014-03-17 14:56:12 -0700

    finished code for adding the low words

--- src/add-32.s
+++ src/add-32.s
@@ -8,6 +8,7 @@ JNC add
 ZERO        DB 0        ; all bits zero
 ONES        DB FFFF     ; all bits one
 ONE         DB 0001     ; one
+TWO         DB 0002     ; two
 
 Alow        DB FEED     ; low part of the first addend
 Ahigh       DB FACE     ; high part of first addend
@@ -24,44 +25,45 @@ carryout    DB 0000     ; carry out
 
 intercarry  DB 0000     ; stores the carry of Alow + Blow
 
+temp        DB 0000     ; temporary storage location
+
 add:
 low:
+NAND ZERO           ; acc = 0xFFFF
+NAND ONES           ; acc = 0x0000
+ST intercarry       ; zero the carry     
+ADD carryin         ; acc = carryin
+
+ADD Alow            ; acc = carryin + Alow
+jnc nextadd1        ; no carry generated, we can continue as normal
+jnc nextadd1_carry  ; a carry was generated, so we need to go to the code that
+                    ; sets intercarry to 1
+
+nextadd1_carry:
+ST temp             ; store Alow + carryin into the temporary
+NAND ZERO           ; acc = 0xffff
+ADD TWO             ; acc = 1
+ST intercarry       ; intercarry = 1 
+
 NAND ZERO
 NAND ONES
+ADD temp            ; acc = Alow + carryin
 
-ADD carryin
-
-ADD Alow
+nextadd1:
 ADD Blow
 
-ST  ResLow
+ST ResLow
 
-JNC nocarrylow
-JNC carrylow
+jnc nextadd2        ; no carry generated
+jnc nextadd2_carry  ; carry generated, need to set intercarry = 1 
 
 
-nocarrylow:
+nextadd2_carry:
 NAND ZERO
-NAND ONES
+ADD TWO
 ST intercarry
-JNC high
-JNC high
 
-carrylow:
-NAND ZERO
-NAND ONES
-ADD ONE
-ST intercarry
-JNC high
-JNC high
 
-high:
-NAND ZERO
-NAND ONES
-ADD intercarry
-ADD Ahigh
-ADD Bhigh
+nextadd2:
 
-ST ResHigh
 
-JNC
